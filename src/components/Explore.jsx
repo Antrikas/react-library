5f50c67ae4a9a2107aeec75a88173964d87e5ee5
Antrikas <andreas.antoniades17@gmail.com>
2023-12-28 15:43:20 +0200

    lrp

---
+++ src/components/Explore.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Explore = () => {
+    return (
+      <section id="id">
+        <div className="container">
+          <div className="row row__column">
+            <h2 className="section__title">
+              Explore more <span className="purple">Books</span>
+            </h2>
+            <BestBooks />
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Explore;
